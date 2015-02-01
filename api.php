1f8cedb8bd5517652eef0fe27a9552b574a615e2
Lucian Hontau <lucian303@gmail.com>
2015-02-01 12:25:07 -0500

    Make auth API call.

--- api.php
+++ api.php
@@ -1,2 +1,22 @@
 <?php
 
+function authenticate()
+{
+    $url = "https://api.expensify.com?command=Authenticate&partnerName=applicant&partnerPassword=d7c3119c6cdab02d68d9&partnerUserID=expensifytest@mailinator.com&partnerUserSecret=hire_me&useExpensifyLogin=true";
+
+    $ch = curl_init();
+    $headers = [
+        'Accept: application/json',
+    ];
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    return $response;
+}
+
+print authenticate();
