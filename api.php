f84b16b2f4dfa62cafa52999db43d38dc7b14fa3
Lucian Hontau <lucian303@gmail.com>
2015-02-05 19:51:41 -0500

    Route input.

--- api.php
+++ api.php
@@ -1,5 +1,34 @@
 <?php
 
+init();
+
+/**
+ * Main script body. Makes different calls depending on input params
+ */
+function init()
+{
+    $output = '';
+
+    $command = $_GET['command'];
+    switch ($command) {
+        case 'Authenticate':
+            $user = $_GET['user'];
+            $pass = $_GET['pass'];
+            $output = authenticate($user, $pass);
+            break;
+    }
+
+    print $output;
+}
+
+/**
+ * Authenticate with the expensify API
+ *
+ * @param $user
+ * @param $pass
+ *
+ * @return mixed
+ */
 function authenticate($user, $pass)
 {
     $url = "https://api.expensify.com?command=Authenticate&partnerName=applicant&partnerPassword=d7c3119c6cdab02d68d9&partnerUserID=$user&partnerUserSecret=$pass";
@@ -7,6 +36,12 @@ function authenticate($user, $pass)
     return makeCall($url);
 }
 
+/**
+ * Make a CURL call to a URL
+ * @param $url
+ *
+ * @return mixed
+ */
 function makeCall($url)
 {
     $headers = [
@@ -22,5 +57,3 @@ function makeCall($url)
 
     return curl_exec($ch);
 }
-
-print authenticate('expensifytest@mailinator.com', 'hire_me');
